2821013e6cc89597cddb718e357307a3015dbc55
himanshukumar76 <himanshukumar75655893@gmail.com>
2025-02-10 23:25:54 +0530

    some new files added and updated

---
+++ OWN DOT NET/data_type/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class data_type
+{
+        static void Main(string[] args)
+    {
+        /*
+        //size of different data types 
+
+        //signed 
+
+        //byte
+        Console.WriteLine("The size of the byte data type is : " + sizeof(byte)*8 + " bits"); //1
+        //sbyte
+        Console.WriteLine("The size of the sbyte data type is : " + sizeof(sbyte)*8 + " bits"); //1
+        //int
+        Console.WriteLine("The size of the int data type is : " + sizeof(int) * 8 + " bits"); //4
+        //short
+        Console.WriteLine("The size of the short data type is : " + sizeof(short) * 8 + " bits"); //2
+        //long
+        Console.WriteLine("The size of the long data type is : " + sizeof(long) * 8 + " bits"); // 8
+        //float
+        Console.WriteLine("The size of the float data type is : " + sizeof(float) * 8 + " bits"); //4
+        //double
+        Console.WriteLine("The size of the double data type is : " + sizeof(double) * 8 + " bits"); //8
+        //decimal
+        Console.WriteLine("The size of the decimal data type is : " + sizeof(decimal) * 8 + " bits"); // 16
+        //char
+        Console.WriteLine("The size of the char data type is : " + sizeof(char) * 8 + " bits"); // 2
+        //boolean
+        Console.WriteLine("The size of the boolean data type is : " + sizeof(bool) * 8 + " bits"); //1
+
+        //unsigned
+
+        //uint
+        Console.WriteLine("The size of the uint data type is : " + sizeof(uint) * 8 + " bits"); //4
+        //ushort
+        Console.WriteLine("The size of the ushort data type is : " + sizeof(ushort) * 8 + " bits"); //2
+        //ulong
+        Console.WriteLine("The size of the ulong data type is : " + sizeof(ulong) * 8 + " bits"); //8
+        */
+
+        // Min and Max Values Of All Data Types 
+        //Signed
+        // sbyte 
+        Console.WriteLine("Sbyte: Min value : " + sbyte.MinValue + " and Max Value : " + sbyte.MaxValue);
+        // short 
+        Console.WriteLine("Short: Min value : " + short.MinValue + " and Max Value : " + short.MaxValue);
+        //int
+        Console.WriteLine("Int: Min value : " + int.MinValue + " and Max Value : " + int.MaxValue);
+        //char
+        Console.WriteLine("Char: Min value : " + char.MinValue + " and Max Value : " + char.MaxValue);
+        //float
+        Console.WriteLine("Float: Min value : " + float.MinValue + " and Max Value : " + float.MaxValue);
+        //double
+        Console.WriteLine("Double: Min value : " + double.MinValue + " and Max Value : " + double.MaxValue);
+        //long
+        Console.WriteLine("Long: Min value : " + long.MinValue + " and Max Value : " + long.MaxValue);
+
+        // unsigned
+        //byte
+        Console.WriteLine("Byte: Min value : " + byte.MinValue + " and Max Value : " + byte.MaxValue);
+        //uint
+        Console.WriteLine("Uint: Min value : " + uint.MinValue + " and Max Value : " + uint.MaxValue);
+        //ulong
+        Console.WriteLine("Ulong: Min value : " + ulong.MinValue + " and Max Value : " + ulong.MaxValue);
+        //ushort
+        Console.WriteLine("Ushort: Min value : " + ushort.MinValue + " and Max Value : " + ushort.MaxValue);
+        }
+}
---
+++ OWN DOT NET/data_type/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("data_type")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("data_type")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3df34b1-e21a-4bef-b10b-5881205e0038")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
