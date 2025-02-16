dc5ebc743cf7d574b11efc275f7451e7b098fbd1
himanshukumar76 <himanshukumar75655893@gmail.com>
2025-02-16 18:42:01 +0530

    some new files added and updated

---
+++ OWN DOT NET/OperatorsAndExpressions/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+class OperatorsAndExpressions
+{
+		static void Main(string[] args)
+		{
+		//OPERATORS : Operators are special type of symbol which perform a specific operations.
+
+		//1. Arithmetic Operator
+
+		int a = 105;
+		int b = 20;
+		Console.WriteLine("a + b : " + (a + b));
+		Console.WriteLine("a - b : " + (a - b));
+		Console.WriteLine("a * b : " + (a * b));
+		Console.WriteLine("a /b : " + (a / b));
+		Console.WriteLine("a % b : " + (a % b));
+
+		//2. Relational Operators
+
+		//int a = 1;	
+		//int b = 2;
+
+		//Console.WriteLine("a == b : "+(a == b));
+		//      Console.WriteLine("a != b : " + (a != b));
+		//      Console.WriteLine("a > b : " + (a > b));
+		//      Console.WriteLine("a < b : " + (a < b));
+		//      Console.WriteLine("a >= b : " + (a >= b));
+		//      Console.WriteLine("a <= b : " + (a <= b));
+
+		//3. Assignment Operator 
+
+		//int a = 2;
+
+		//Console.WriteLine("a = "+a);
+		//      Console.WriteLine("a += 1 = " + (a += 1));
+		//      Console.WriteLine("a -= 1 = " + (a -= 1));
+		//      Console.WriteLine("a *= 2 = " + (a *= 2));
+		//      Console.WriteLine("a /= 2 = " + (a /= 2));
+		//      Console.WriteLine("a %= 2 = " + (a %= 2));
+
+		//4. Logical Operator
+
+		//bool a = true;
+		//bool b = false;
+
+		//Console.WriteLine("a && a = " +(a && a));
+		//      Console.WriteLine("a && b = " + (a && b));
+		//      Console.WriteLine("b && b = " + (b && b));
+		//      Console.WriteLine("a || a = " + (a || a));
+		//      Console.WriteLine("a || b = " + (a || b));
+		//      Console.WriteLine("b || a = " + (b || a));
+		//      Console.WriteLine("b || b = " + (b || b));
+		//      Console.WriteLine("!a = " + (!a));
+
+		//5. Increament And Decrement Operator
+
+		//int a = 1;
+
+		//Console.WriteLine("a = "+a);
+		//Console.WriteLine("a++ = "+(a++));
+		//      Console.WriteLine("a = " + a);
+		//Console.WriteLine("++a = " + (++a));
+		//      Console.WriteLine("a = " + a);
+
+		//6. Bitwise Operators
+
+		//int a = 10;
+		//int b = 2;
+
+		//Console.WriteLine("a & b = " + (a & b));
+		//      Console.WriteLine("a | b = " + (a | b));
+		//      Console.WriteLine("a ^ b = " + (a ^ b));
+		//Console.WriteLine("a & b = " + (~a));
+		//      Console.WriteLine("a << b = " + (a << b));
+		//      Console.WriteLine("a >> b = " + (a >> b));
+
+		//7. Ternary Operator
+
+		//int a = 2;
+		//int b = 3;
+
+		//int ans = a > b ? a : b; 
+		//Console.WriteLine(ans);
+
+
+
+
+	}
+}
